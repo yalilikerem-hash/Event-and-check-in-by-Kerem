daea83ce4574828bedae2969836a64cc202a13f9
yalilikerem-hash <yalilikerem@gmail.com>
2025-12-16 23:37:11 +0300

    Initialize event_info with event details
    Add event_info dictionary with details for an event.

--- events.py
+++ events.py
@@ -1,1 +1,15 @@
+import random
+from datetime import datetime
+id = random.randint(10,812)
 
+event_info = {
+    "id":   id ,
+    "name": "Kerem Yalılı",
+    "location": "Nostalji Düğün Salonu",
+    "start_date": datetime(8,16,2007) ,
+    "end_date":  datetime(8,18,2007),
+    "capacity": 500,
+    "price": 100000               
+}
+   
+def load_events():
