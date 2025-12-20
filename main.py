8a317e122fad92f1f25f05b4457d07447bc5c185
yalilikerem-hash <yalilikerem@gmail.com>
2025-12-20 21:06:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+while True:
+        print("\n--- EVENT PLATFORM ---")
+        print("1. Organizer Menu\n2. Staff (Check-in)\n3. Attendee Menu\n4. Exit")
+        role = input("Select Role: ")
+
+        if role == '1': 
+            print("\n1. Create Event\n2. View Reports\n3. Backup Data")
+            choice = input("Choice: ")
+            if choice == '1':
+                name = input("Event Name: ")
+                cap = int(input("Capacity: "))
+                price = float(input("Price: "))
