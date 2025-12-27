ae602bc81da74a6a670e1f80001123945acd7cc4
yalilikerem-hash <yalilikerem@gmail.com>
2025-12-27 23:14:02 +0300

    Add check_in function for registration process

--- checkin.py
+++ checkin.py
@@ -1,1 +1,10 @@
-
+def check_in(regs, reg_id):
+    for r in regs:
+        if r['id'] == reg_id:
+            if r['status'] == "Onaylı":
+                r['checked_in'] = True
+                return "Giriş Başarılı!"
+            else:
+                return "Hata: Bekleme listesindekiler giriş yapamaz."
+    return "Kayıt bulunamadı."
+  
